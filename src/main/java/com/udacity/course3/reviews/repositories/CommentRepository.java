01395ecf7888b37401a04da81acdb1a2cba68567
kratipaw <kratipaw3@gmail.com>
2019-09-01 22:53:47 +0530

    Implemented the repositories for Product, Review, Comment

---
+++ src/main/java/com/udacity/course3/reviews/repositories/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.udacity.course3.reviews.repositories;
+
+import com.udacity.course3.reviews.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    List<Comment> findAllByReviewId(Integer reviewId);
+}
---
+++ src/main/java/com/udacity/course3/reviews/repositories/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.udacity.course3.reviews.repositories;
+
+import com.udacity.course3.reviews.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+
+    List<Review> findAllByProductId(Integer productId);
+}
